734c38656543eba82efd27e71e5ad7ef7cf32144
NoIgnore <1209172438@qq.com>
2022-04-14 14:55:24 +0800

    Site updated: 2022-04-14 14:55:22

---
+++ qwe/precache-manifest.1404baecdc047d9348aae1668bd1339c.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4db9a83020160cbe840c69d6c9bca376",
+    "url": "android-chrome-192x192.png"
+  },
+  {
+    "revision": "d31dbf8ecc438a2dace0370619158d6d",
+    "url": "android-chrome-512x512.png"
+  },
+  {
+    "revision": "4db9a83020160cbe840c69d6c9bca376",
+    "url": "android-chrome-maskable-192x192.png"
+  },
+  {
+    "revision": "d31dbf8ecc438a2dace0370619158d6d",
+    "url": "android-chrome-maskable-512x512.png"
+  },
+  {
+    "revision": "0c7bf7222cb4db061f86802a89781e33",
+    "url": "apple-touch-icon.png"
+  },
+  {
+    "revision": "761b7d8bf99f73ee77ce",
+    "url": "css/app.316c004d.css"
+  },
+  {
+    "revision": "ba4bc7dbcc1ec37392eb",
+    "url": "css/chunk-vendors.09b7d566.css"
+  },
+  {
+    "revision": "3f3d05a292cbcb10d4dbab862f5347bd",
+    "url": "favicon-16x16.png"
+  },
+  {
+    "revision": "1474e156fab8afa33501236d63c01b17",
+    "url": "favicon-32x32.png"
+  },
+  {
+    "revision": "94ac5ed9391071e35bf5133c3cf4e401",
+    "url": "img/add.94ac5ed9.svg"
+  },
+  {
+    "revision": "6e72989e6889cc9519bd410a32188bb8",
+    "url": "img/album.6e72989e.svg"
+  },
+  {
+    "revision": "64b50ba65882a62e8767fc5e57f680bd",
+    "url": "img/avatars.64b50ba6.jpg"
+  },
+  {
+    "revision": "42ae222066331669cca3d04e6e73d6d2",
+    "url": "img/chat-green.42ae2220.svg"
+  },
+  {
+    "revision": "0d4819e370b48192a96a9735c4da5a4f",
+    "url": "img/chat.0d4819e3.svg"
+  },
+  {
+    "revision": "091a88e3e7c0d95f923bb67214b6d076",
+    "url": "img/close.091a88e3.svg"
+  },
+  {
+    "revision": "38fed3ee5418b12d5904dd0c1c88bf9b",
+    "url": "img/compass.38fed3ee.svg"
+  },
+  {
+    "revision": "5782d94e141aa1f442550d57d7b73646",
+    "url": "img/contact.5782d94e.svg"
+  },
+  {
+    "revision": "ad3b00d8df753de24e98e54a8c9b1e58",
+    "url": "img/friendadd-fill.ad3b00d8.svg"
+  },
+  {
+    "revision": "0ed7fb73b3d504d1ef8c4d55accee723",
+    "url": "img/logo.0ed7fb73.png"
+  },
+  {
+    "revision": "3d15a57889c5708c30c9f01461226533",
+    "url": "img/mail-fill.3d15a578.svg"
+  },
+  {
+    "revision": "4ba4c92b3f2b3a502b5c8432d277accd",
+    "url": "img/more.4ba4c92b.svg"
+  },
+  {
+    "revision": "0229273300068a45ed8f2d586ab6d623",
+    "url": "img/pay.02292733.svg"
+  },
+  {
+    "revision": "39629214f1507b7696c3b472e8d0cac2",
+    "url": "img/qr-icon.39629214.svg"
+  },
+  {
+    "revision": "4c8288202c422076e14637f60410368e",
+    "url": "img/qr.4c828820.jpg"
+  },
+  {
+    "revision": "ea504b49bb13a43051213cc09dab1af8",
+    "url": "img/scan.ea504b49.svg"
+  },
+  {
+    "revision": "8ab806620d688d28e77030a04d27de10",
+    "url": "img/search.8ab80662.svg"
+  },
+  {
+    "revision": "50ddf8945e6b5fc1d2a818907d9b48b9",
+    "url": "img/user.50ddf894.svg"
+  },
+  {
+    "revision": "03883c8e452e0058b1182595974235a4",
+    "url": "img/wechat.03883c8e.svg"
+  },
+  {
+    "revision": "8f17efcb3fcf5baa8aa91230ddc78765",
+    "url": "index.html"
+  },
+  {
+    "revision": "761b7d8bf99f73ee77ce",
+    "url": "js/app.74ca3475.js"
+  },
+  {
+    "revision": "ba4bc7dbcc1ec37392eb",
+    "url": "js/chunk-vendors.f4b281a1.js"
+  },
+  {
+    "revision": "3d5e3315eafeaf172574ce2b9ed94e7c",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
